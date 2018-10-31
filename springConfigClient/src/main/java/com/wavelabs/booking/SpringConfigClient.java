441ecadd8f29deb6004f43511df1546bd4105398
radharukhminia <44636898+radharukhminia@users.noreply.github.com>
2018-10-31 17:55:27 +0530

    Add files via upload

---
+++ springConfigClient/src/main/java/com/wavelabs/booking/SpringConfigClient.java
@@ -0,0 +1,32 @@
+package com.wavelabs.booking;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringConfigClient {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigClient.class, args);
+
+	}
+}
+
+@RefreshScope
+@RestController
+@ConfigurationProperties("user")
+class MessageRestController {
+
+	@Value("${user.name}")
+	 String name;
+
+	@RequestMapping("/msg")
+	String getMsg() {
+		return this.name;
+	}
+}
---
+++ springConfigClient/src/test/java/com/wavelabs/booking/SpringConfigClientTests.java
@@ -0,0 +1,16 @@
+package com.wavelabs.booking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringConfigClientTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
