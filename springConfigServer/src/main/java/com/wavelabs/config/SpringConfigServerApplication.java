5698ed7bc7fc310de9c7427996c6edaeddac912a
radharukhminia <44636898+radharukhminia@users.noreply.github.com>
2018-11-01 14:34:46 +0530

    Update SpringConfigServerApplication.java

--- springConfigServer/src/main/java/com/wavelabs/config/SpringConfigServerApplication.java
+++ springConfigServer/src/main/java/com/wavelabs/config/SpringConfigServerApplication.java
@@ -3,7 +3,7 @@ package com.wavelabs.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
+@RefreshScope
 @EnableConfigServer 
 @SpringBootApplication
 public class SpringConfigServerApplication {
