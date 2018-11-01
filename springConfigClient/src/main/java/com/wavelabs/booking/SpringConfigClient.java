aae731539040ae414e8f50549d78d56224602912
radharukhminia <44636898+radharukhminia@users.noreply.github.com>
2018-11-01 14:33:38 +0530

    Update SpringConfigClient.java

--- springConfigClient/src/main/java/com/wavelabs/booking/SpringConfigClient.java
+++ springConfigClient/src/main/java/com/wavelabs/booking/SpringConfigClient.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@RefreshScope
 @SpringBootApplication
 public class SpringConfigClient {
 
